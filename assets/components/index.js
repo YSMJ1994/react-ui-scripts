fdb3ead6ae3983c7646c7bf81eb4ea5a511d9d53
zhaopengcheng <710411486@qq.com>
2019-10-14 17:09:50 +0800

    clean assets

--- assets/components/index.js
+++
@@ -1,9 +0,0 @@
-import Comp_0 from './Button';
-
-export default [Comp_0].sort((a, b) => {
-  if (a.config.order === b.config.order) {
-    return a.config.name >= b.config.name ? 1 : -1;
-  } else {
-    return a.config.order - b.config.order;
-  }
-});
