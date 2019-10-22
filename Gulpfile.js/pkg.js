a3cd35909cceaa331252535d757b3a1a85b707a7
zhaopengcheng <710411486@qq.com>
2019-10-22 16:29:14 +0800

    lib pkg files field fix

--- Gulpfile.js/pkg.js
+++ Gulpfile.js/pkg.js
@@ -5,6 +5,7 @@ const paths = require("../config/paths");
 const cruConfig = require("../config/config");
 const { libraryBuild, targetPkg, libraryStatic } = paths;
 const { writeJSON, exists, copyDir } = require("../utils/fs");
+const { arrayDuplicateRemoval } = require("../utils");
 const { src, dest, series, parallel } = require("gulp");
 const targetPkgJson = require(targetPkg);
 const isTs = cruConfig.typescript;
@@ -13,7 +14,9 @@ async function generatePkg() {
   const json = JSON.parse(JSON.stringify(targetPkgJson));
   Reflect.deleteProperty(json, "scripts");
   Reflect.deleteProperty(json.dependencies, "react-ui-scripts");
-  json.files = ["es", "lib"];
+  json.files = [...(json.files || []), "es", "lib", "typings"];
+  // 去重
+  arrayDuplicateRemoval(json.files);
   json.main = "lib/index.js";
   json["module"] = "es/index.js";
   json.license = "MIT";
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
-const nodePath = require('path')
+const nodePath = require("path");
 const delayTagMap = new Map();
 
 const executeDelay = (function() {
@@ -9,13 +9,41 @@ const executeDelay = (function() {
 })();
 
 function path2GulpPath(path) {
-    if(!path || typeof path !== 'string') {
-        throw new Error(`invalid argument: path[ ${path} ]`);
+  if (!path || typeof path !== "string") {
+    throw new Error(`invalid argument: path[ ${path} ]`);
+  }
+  return path.split(nodePath.sep).join("/");
+}
+
+/**
+ * 数组去重
+ * @param arr 目标数组 改变原数组
+ * @return {[]} 删除的元素组成的数组
+ */
+function arrayDuplicateRemoval(arr) {
+  if (typeof arr !== "object" || !Array.isArray(arr)) {
+    throw new Error(`arr must been an array`);
+  }
+  const cache = new Map();
+  let i = 0;
+  const deleteArr = [];
+  while (i < arr.length) {
+    const item = arr[i];
+    if (cache.has(item)) {
+      // 已存在，则删除该位置的元素，
+      const dArr = arr.splice(i, 1);
+      deleteArr.push.apply(deleteArr, dArr);
+    } else {
+      // 缓存中没有，则缓存该元素，并继续下一次
+      cache.set(item, true);
+      i++;
     }
-    return path.split(nodePath.sep).join('/');
+  }
+  return deleteArr;
 }
 
 module.exports = {
   executeDelay,
-    path2GulpPath
+  path2GulpPath,
+  arrayDuplicateRemoval
 };
