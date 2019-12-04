a3ace74075ca6a8b82e04b51944e607a50ecfe8c
zhaopengcheng <710411486@qq.com>
2019-12-04 10:07:26 +0800

    clean

--- scripts/compileComponents.js
+++ scripts/compileComponents.js
@@ -4,7 +4,6 @@ const paths = require("../utils/paths");
 const cruConfig = require("../config/config");
 const path = require("path");
 const { executeDelay } = require("../utils");
-const HighLight = require("highlight.js");
 const channel = require("./channel");
 const {
   readDir,
@@ -12,34 +11,14 @@ const {
   isFile,
   readFile,
   writeFile,
-  removeFile,
   emptyDir,
-  removeFileAsync,
   getFilename,
   exists
 } = require("../utils/fs");
-const getMDT = require("./MDT");
-const MDT = getMDT();
-
-function resolveHTMLToJSX(html) {
-  return String(html)
-    .replace(/class="/g, 'className="')
-    .replace(/style="([^"]*)"/g, "style={}")
-    .replace(/__cls/g, "class");
-}
-
-function hasImportReact(code) {
-  if (!code) {
-    return false;
-  }
-  const match = String(code).match(/import\s+(.+)\s+from\s+['"]react['"];?/);
-  return !!match && !!String(match[1]).match(/React/);
-}
 
 const {
   componentRoot,
   assetsComponentRoot,
-  targetPkgName,
   assetsStyleRoot
 } = paths;
 
@@ -47,203 +26,6 @@ let componentsCompileArr = [];
 const components = {};
 const generateIndexTask = [];
 const suffix = cruConfig.typescript ? "tsx" : "jsx";
-const configRegExp = /^---([\s\S]*?)---$/im;
-const demoReplaceRegExp = /^<!--\s*demo\s*-->$/im;
-const demoConfigRegExp = /^---$/im;
-const demoCodeRegExp = /^```.*jsx.*\n([\s\S]*?)```$/im;
-const demoCssCodeRegExp = /^```(css|sass|scss|less).*\n([\s\S]*?)```$/im;
-
-const defaultDemoCode = "export default () => null;";
-
-function getConfig(configStr, filename) {
-  const indexConfigMapRegExp = /(order|type|name|sub)[:：]\s*([^\s\n]*)/gi;
-  if (!configStr) {
-    return {
-      order: 0,
-      type: "其他",
-      name: filename,
-      sub: ""
-    };
-  } else {
-    let config = {};
-    let match;
-    while ((match = indexConfigMapRegExp.exec(configStr))) {
-      const [, key, value = ""] = match;
-      config[key] = String(value).trim();
-    }
-    if (!config.name) {
-      config.name = filename;
-    }
-    if (!config.order) {
-      config.order = 0;
-    }
-    if (!config.type) {
-      config.type = "其他";
-    }
-    if (!config.sub) {
-      config.sub = "";
-    }
-    if (typeof config.order !== "number") {
-      config.order = isNaN(+config.order) ? 0 : +config.order;
-    }
-    return config;
-  }
-}
-
-function getDemoConfig(configStr, filename) {
-  const configMapRegExp = /(order|title)[:：]\s*([^\s\n]*)/gi;
-  if (!configStr) {
-    return {
-      order: 0,
-      title: filename
-    };
-  } else {
-    let config = {};
-    let match;
-    while ((match = configMapRegExp.exec(configStr))) {
-      const [, key, value = ""] = match;
-      config[key] = String(value).trim();
-    }
-    if (!config.title) {
-      config.title = filename;
-    }
-    if (!config.order) {
-      config.order = 0;
-    }
-    if (typeof config.order !== "number") {
-      config.order = isNaN(+config.order) ? 0 : +config.order;
-    }
-    return config;
-  }
-}
-
-async function deleteExtraFile(dir, retainFiles) {
-  if (!exists(dir) || !isDir(dir)) {
-    return;
-  }
-  // console.log("dir", dir);
-  // console.log("retainFiles", retainFiles);
-  const children = await readDir(dir);
-  const retainObj = retainFiles.reduce((pre, name) => {
-    pre[name] = true;
-    return pre;
-  }, {});
-  await Promise.all(
-    children
-      .filter(name => !retainObj[name])
-      .map(name => removeFileAsync(path.resolve(dir, name)))
-  );
-}
-
-async function writeComp(comp) {
-  const {
-    indexInfo: { config, html, writeBase },
-    demoInfo
-  } = comp;
-  const compBase = config.id;
-  const demoStrArr = [];
-  const demoCompCodeArr = [];
-  const demoCssCodeArr = [];
-  const demos = Object.keys(demoInfo).reduce((pre, key) => {
-    pre.push(demoInfo[key]);
-    return pre;
-  }, []);
-  demos.forEach((demo, i) => {
-    const {
-      filename,
-      id,
-      order,
-      title,
-      description,
-      code,
-      css,
-      cssSuffix
-    } = demo;
-    const name = getFilename(filename, ".md");
-    const demoCompPath = `./demo/${name}.${suffix}`;
-    demoStrArr.push(
-      `{ component: asyncComponent(() => import(/* webpackChunkName: "comp-${
-        config.name
-      }_demo-${name}" */'${demoCompPath}'), withActiveAnchor), config: { id: ${JSON.stringify(
-        id
-      )}, filename: ${JSON.stringify(filename)}, order: ${JSON.stringify(
-        order
-      )}, title: ${JSON.stringify(title)}, description: ${JSON.stringify(
-        description
-      )}, code: ${JSON.stringify(HighLight.highlight("jsx", code).value)} } }`
-    );
-    const cssName = css && `${name}.${cssSuffix}`;
-    demoCompCodeArr.push({
-      filename: `${name}.${suffix}`,
-      code: code,
-      cssName
-    });
-    cssName && demoCssCodeArr.push({ filename: cssName, css });
-  });
-  const writeIndexContent = `import React from 'react';
-import asyncComponent from 'toolSrc/components/asyncComponent';
-import withActiveAnchor from 'toolSrc/hoc/withActiveAnchor';
-
-const demos = [${demoStrArr.join(",\n")}].sort((a, b) => {
-  if (a.config.order === b.config.order) {
-    return a.config.title >= b.config.title ? 1 : -1;
-  } else {
-    return a.config.order - b.config.order;
-  }
-});
-const Comp = asyncComponent(() => import(/* webpackChunkName: "comp-${
-    config.name
-  }" */'./comp'), withActiveAnchor);
-export default {
-    config: {
-        id: ${JSON.stringify(config.id)},
-        order: ${JSON.stringify(config.order)},
-        type: ${JSON.stringify(config.type)},
-        name: ${JSON.stringify(config.name)},
-        sub: ${JSON.stringify(config.sub)},
-        demos: demos
-    },
-    component: () => (
-        <Comp demos={demos}/>
-    )
-}`;
-  const writeCompContent = `import React from 'react';
-import DemoWrap from "toolSrc/components/DemoWrap";
-
-export default ({demos}) => (<article>${html}</article>);
-`;
-  // console.log("writeBase", writeBase);
-  const demoWriteBase = path.resolve(writeBase, "demo");
-  // write demos
-  await Promise.all(
-    demoCompCodeArr.map(({ filename, code, cssName }) =>
-      writeFile(
-        path.resolve(demoWriteBase, filename),
-        `${hasImportReact(code) ? "" : `import React from 'react'`};\n${
-          cssName ? `import './${cssName}';\n` : "\n"
-        }${code}`
-      )
-    )
-  );
-  await Promise.all(
-    demoCssCodeArr.map(({ filename, css }) =>
-      writeFile(
-        path.resolve(demoWriteBase, filename),
-        `@charset "UTF-8";\n${css}`
-      )
-    )
-  );
-  // write component index
-  const writeIndexPath = path.resolve(writeBase, `index.${suffix}`);
-  const writeCompPath = path.resolve(writeBase, `comp.${suffix}`);
-  await writeFile(writeCompPath, writeCompContent);
-  await writeFile(writeIndexPath, writeIndexContent);
-  // 清空demo目录内多余的文件
-  await deleteExtraFile(
-    demoWriteBase,
-    [...demoCompCodeArr, ...demoCssCodeArr].map(d => d.filename)
-  );
-}
 
 async function parseOne(compBase) {
   const compBaseName = getFilename(compBase);
